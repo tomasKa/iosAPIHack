236d0c6a05c5abb91fe8b0dd28a7f0975d692b24
Tomas Kamarauskas <tomas@kamatom.com>
2015-04-21 16:22:49 +0200

    fetching images according to activity

---
+++ iosAPIHAck/photoCellCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  photoCellCollectionViewCell.h
+//  iosAPIHAck
+//
+//  Created by Tomas Kamarauskas on 21/04/15.
+//  Copyright (c) 2015 EEVOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface photoCellCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ iosAPIHAck/photoCellCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  photoCellCollectionViewCell.m
+//  iosAPIHAck
+//
+//  Created by Tomas Kamarauskas on 21/04/15.
+//  Copyright (c) 2015 EEVOL. All rights reserved.
+//
+
+#import "photoCellCollectionViewCell.h"
+
+@implementation photoCellCollectionViewCell
+
+@end
